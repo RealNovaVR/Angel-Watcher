3ed0326a8eb1c0ab91a14ec006a6bc5ef3b8faf9
RealNovaVR <demonicprmusic@gmail.com>
2026-09-22 09:58:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -393,4 +393,23 @@ client.once(Events.ClientReady, readyClient => {
 // LOGIN
 // =========================
 
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+    res.end("Discord moderation bot is online!");
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Web server listening on port ${PORT}`);
+});
+
 client.login(process.env.DISCORD_TOKEN);
+
+
+
