4d1f2196bf8eb94c6f7fcdd5d2a2a3b2cc697a16
RealNovaVR <demonicprmusic@gmail.com>
2026-09-22 11:23:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 const {
     Client,
     GatewayIntentBits,
